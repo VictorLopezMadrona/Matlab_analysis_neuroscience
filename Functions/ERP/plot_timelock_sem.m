23a2989ef69d7dcc68e87b04f0bd0c925fa3ecf1
VictorLopezMadrona <VictorLopezMadrona@users.noreply.github.com>
2024-11-27 09:56:41 +0100

    Update plot_timelock_sem.m

--- Functions/ERP/plot_timelock_sem.m
+++ Functions/ERP/plot_timelock_sem.m
@@ -16,6 +16,15 @@ function plot_timelock_sem(cnfg,timelock)
 % Log:
 % 03.09.2024: Corrected an error when cfg.time was a column and not a row
 
+% Default Matlab colors
+%[0 0.4470 0.7410]	"#0072BD"	
+%[0.8500 0.3250 0.0980]	"#D95319"	
+%[0.9290 0.6940 0.1250]	"#EDB120"	
+%[0.4940 0.1840 0.5560]	"#7E2F8E"	
+%[0.4660 0.6740 0.1880]	"#77AC30"	
+%[0.3010 0.7450 0.9330]	"#4DBEEE"	
+%[0.6350 0.0780 0.1840]
+
 if ~isfield(cnfg,'color')
     color=[0.8 0.8 0.8];
 else
