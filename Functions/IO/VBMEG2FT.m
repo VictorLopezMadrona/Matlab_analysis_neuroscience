65f7079f429d8373e9fbb1cbbed0a5e9d9ed8d82
VictorLopezMadrona <VictorLopezMadrona@users.noreply.github.com>
2025-03-12 16:54:16 +0900

    Include electrode position as output

--- Functions/IO/VBMEG2FT.m
+++ Functions/IO/VBMEG2FT.m
@@ -1,5 +1,5 @@
 
-function ftdata = VBMEG2FT(filename)
+function [ftdata, elec_pick] = VBMEG2FT(filename)
 
 % Load a VBMEG file and convert it to FieldTrip. Continuous data, no trials 
 %
@@ -14,6 +14,7 @@ ftdata.label = vbdata.MEGinfo.ChannelInfo.Name;
 ftdata.trial{1} = vbdata.bexp;
 ftdata.fsample = vbdata.MEGinfo.SampleFreq;
 ftdata.time{1} = linspace(0,size(ftdata.trial{1},2)/ftdata.fsample,size(ftdata.trial{1},2));
+elec_pick = vbdata.pick;
 
 
 
