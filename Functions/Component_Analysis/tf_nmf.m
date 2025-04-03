52685446ceff50408e86bb5e1c19f4cee1275ba6
VictorLopezMadrona <VictorLopezMadrona@users.noreply.github.com>
2025-04-03 11:35:50 +0900

    Included stats

--- Functions/Component_Analysis/tf_nmf.m
+++ Functions/Component_Analysis/tf_nmf.m
@@ -26,6 +26,8 @@ function [Wtf,H,W] = tf_nmf(cnfg,ftdata)
 %       M          - Time length for Gabor im samples (Def 128)
 %       a          - Gabor resolution (Def M/16)
 %       freqlim    - Frequencies of interest. Ex [5 100]
+%       Nsurro     - Number of surrogates. Def=100
+%       stats      - method for stats: 'cluster' (def), 'pixel'
 %
 %       dosave   - logical. True/false save/not save the results
 %       outpath  - string. Path to save the results if dosave=true
@@ -51,7 +53,9 @@ function [Wtf,H,W] = tf_nmf(cnfg,ftdata)
 if ~isfield(cnfg,'Ncomp'), cnfg.Ncomp = 'velicer'; end
 if ~isfield(cnfg,'M'), cnfg.M = 128; end
 if ~isfield(cnfg,'a'), cnfg.a = cnfg.M/16; end
-    
+if ~isfield(cnfg,'Nsurro'), cnfg.Nsurro = 100; end
+if ~isfield(cnfg,'stats'), cnfg.stats = 'cluster'; end
+
 if ~isfield(cnfg,'infosave'), cnfg.infosave=''; end
 if ~isfield(cnfg,'dosave'), cnfg.dosave=false; end
 if ~isfield(cnfg,'doplot'), cnfg.doplot=true; end
@@ -133,8 +137,112 @@ H0 = abs(S(1:k,:));
 [W, H] = nnmf(V, k, 'w0', W0, 'h0', H0, 'algorithm', 'mult');
 
 %%% Reshape to original dimensions
-[T, F, S] = size(pow_mean);
+[T, F, ~] = size(pow_mean);
 Wtf = reshape(W, [T, F, k]);   % Back to time × freq × sensors
+
+%% STATS
+
+if cnfg.Nsurro > 0
+    disp('Computing surrogates on NMF...')
+    k_surro=1;
+    W0 = abs(U(:,1:k_surro));
+    H0 = abs(S(1:k_surro,:));
+    surro_cluster = zeros(1,cnfg.Nsurro);
+    pixel_max = zeros(1,cnfg.Nsurro);
+    pixel_min = zeros(1,cnfg.Nsurro);
+    THclus_mask = zeros(T,F,k);
+    for si=1:cnfg.Nsurro
+        % Define surrogate trials between the first and the last one at random time
+        % points
+        onset_ini = min(onset_samples);
+        onset_end = max(onset_samples);
+        onset_aux = randperm(onset_end-onset_ini);
+        onset_surro = onset_aux(1:Ntrial)+onset_ini;
+        trl_ini = onset_surro + pre_samples;  % Start sample
+        trl_end = onset_surro + post_samples; % End sample
+        pow_trial_surro = [];
+        for tri=1:Ntrial
+            pow_trial_surro(:,:,:,tri) = pow_corrected(:,trl_ini(tri):trl_end(tri),:);
+        end
+        
+        pow_mean_surro = mean(pow_trial_surro,4);
+        
+        %%% Vectorize
+        [Ts, Fs, Ss] = size(pow_mean_surro);
+        pow_vec = reshape(pow_mean_surro, [Ts*Fs, Ss]);
+        V = pow_vec - min(pow_vec(:));
+        [W_surro, ~] = nnmf(V, k_surro, 'w0', W0, 'h0', H0, 'algorithm', 'mult');
+        
+        %%% For Pixel-based stats %%%
+        pixel_max(si) = max(W_surro(:));
+        pixel_min(si) = min(W_surro(:));
+        
+        %%% For cluster-stats %%%
+        Wtf_surro = reshape(W_surro, [Ts, Fs, 1]);  
+        % I put a thresholf of twice the std
+        th_surro = mean(W_surro)+2*std(W_surro);
+        Wtf_mask = Wtf_surro>=th_surro;
+        % By default I select the maximum value as 'highest cluster'
+        cluster_aux = max(W_surro(:));
+        % Then, I do a loop over all clusters and select its value as the
+        % sumatory of the values inside the cluster
+        CC = bwconncomp(Wtf_mask);
+        SS = regionprops(CC,'Area','PixelIdxList');
+        for cli = 1:length(SS)
+            cluster_aux = [cluster_aux sum(Wtf_surro(SS(cli).PixelIdxList))];
+        end
+        surro_cluster(si) = max(cluster_aux);
+        
+    end
+      
+    if strcmp(cnfg.stats,'pixel')
+        % For each surrogate, I keep the pixel with highest and lowest weight
+        % I define the THs as the 97.5% highest value in pixel_max and 97.5% lowest
+        % value in pixel_min.
+        % For now, only positive values (95%).   
+        pixel_aux = sort(pixel_max);
+        THmax = pixel_aux(round(cnfg.Nsurro*0.95));
+        %pixel_aux = sort(pixel_min);
+        %pixel_aux = pixel_aux(end:-1:1);
+        %THmin = pixel_aux(round(cnfg.Nsurro*0.975)); 
+        disp(['Threshold of significance pixel-based = ' num2str(THmax)])
+    end
+    
+    if strcmp(cnfg.stats,'cluster')
+        % For each surrogate, I keep the biggest cluster (sumatory of values inside)
+        % I define the THs as the 97.5% highest value in pixel_max and 97.5% lowest
+        % value in pixel_min.
+        pixel_aux = sort(surro_cluster);
+        THcluster = pixel_aux(round(cnfg.Nsurro*0.95));
+        
+        for ki=1:k
+            % Cluster Original for each component
+            Wtf_aux = squeeze(Wtf(:,:,ki));
+            % I put a thresholf of twice the std
+            th_orig = mean(Wtf_aux(:))+2*std(Wtf_aux(:));
+            Wtf_mask = Wtf_aux>=th_orig;
+            % Remove those clusters that are not significant
+            CC = bwconncomp(Wtf_mask);
+            SS = regionprops(CC,'Area','PixelIdxList');
+            flag = [];
+            for cli = 1:length(SS)
+                if sum(Wtf_surro(SS(cli).PixelIdxList)) <= THcluster
+                    flag = [flag cli];
+                end
+            end
+            SS(flag)=[];
+            % Create a mask of significance
+            THclus_mask_aux = Wtf_aux*0;
+            for cli = 1:length(SS)
+                THclus_mask_aux(SS(cli).PixelIdxList) = 1;
+            end
+            THclus_mask(:,:,ki) = THclus_mask_aux;
+        end
+    end
+     
+end
+
+
 
 %% PLOT results
 
@@ -144,7 +252,7 @@ if cnfg.dosave
 end
 
 if isfield(cnfg,'freqlim')
-    ff = linspace(cnfg.freqlim(1),cnfg.freqlim(2),size(c,1));
+    ff = linspace(cnfg.freqlim(1),cnfg.freqlim(2),size(Wtf,1));
 else
     ff = linspace(0,Fs/2,size(c,1));
 end
@@ -160,8 +268,30 @@ for iter=1:k
     ncols = ceil(k/nrows);
     G(iter) = subplot(nrows,ncols,iter); 
     G(iter).ButtonDownFcn = @newFigure1;
-
-    P=imagesc(tt,ff,squeeze(Wtf(:,:,iter)));
+    hold on,
+    Witer = squeeze(Wtf(:,:,iter));
+    P=imagesc(tt,ff,Witer);
+    
+    if strcmp(cnfg.stats,'pixel')
+        mask_max = Witer>=THmax;
+        B = bwboundaries(mask_max);
+        for i=1:length(B)
+            plot(tt(B{i}(:,2)),ff(B{i}(:,1)),'k','LineWidth',1.5),
+        end
+        %mask_min = Witer<THmin;
+        %B = bwboundaries(mask_min);
+        %for i=1:length(B)
+        %    plot(tt(B{i}(:,2)),ff(B{i}(:,1)),'k'),
+        %end
+    end
+    if strcmp(cnfg.stats,'cluster')
+        mask_max = squeeze(THclus_mask(:,:,iter));
+        B = bwboundaries(mask_max);
+        for i=1:length(B)
+            plot(tt(B{i}(:,2)),ff(B{i}(:,1)),'k','LineWidth',1.5),
+        end
+    end
+    
     
     %colormap 'winter'
     xlabel('Time (s)')
@@ -169,13 +299,13 @@ for iter=1:k
     set(P,'UserData',iter);
     set(P, 'HitTest', 'off');
     axis xy
-    %axis([cnfg.toi(1) cnfg.toi(end) cnfg.foilim(1) cnfg.foilim(end)])
+    axis([tt(1) tt(end) ff(1) ff(end)])
     %caxis([-max(abs(itc.tf_corrected(:))) max(abs(itc.tf_corrected(:)))])
     title(['TF Comp - ' num2str(iter)]); 
     colorbar
 end
 % Maximize the figure window to fill the screen
-set(h, 'Units', 'normalized', 'OuterPosition', [0 0 1 1]);
+%set(h, 'Units', 'normalized', 'OuterPosition', [0 0 1 1]);
     if cnfg.dosave
         savefig(h,[cnfg.outpath 'NMF_Wtf' cnfg.infosave ])
         saveas(h,[cnfg.outpath 'NMF_Wtf' cnfg.infosave '.png'])
